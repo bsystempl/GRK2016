3e81417b49a0c693323e3d8d83f3b40c6facea3b
Paweł Urbaniak <Pawe Urbaniak>
2016-03-16 12:25:16 +0100

    chyba bedzie dzialac

--- Main.cpp
+++ Main.cpp
@@ -4,29 +4,53 @@
 #include "Shader_Loader.h"
 using namespace Core;
 GLuint program;
+GLuint vbo, vao;
 
+/*
 void Init()
 {
 	glEnable(GL_DEPTH_TEST);
 	//za³aduj i skompiluj shadery
 	Core::Shader_Loader shaderLoader;
-	program = shaderLoader.CreateProgram("Vertex_Shader.glsl", "Fragment_Shader.glsl");
+	//program = shaderLoader.CreateProgram("Vertex_Shader.glsl", "Fragment_Shader.glsl");
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-}
+} */
 
 void renderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.3, 0.3, 1.0);
 	//u¿ywaj wprowadzone shadery
-	glUseProgram(program);
+	//glUseProgram(program);
 	//narysuj trzy trojkaty
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glutSwapBuffers();
 }
 
+const float vertexPositions[] = {
+	0.25f, 0.25f, 0.0f, 1.0f,
+	0.25f, -0.25f, 0.0f, 1.0f,
+	-0.25f, -0.25f, 0.0f, 1.0f,
+};
+
+void init2(void)
+{
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindVertexArray(vao);
+}
+
 int main(int argc, char **argv)
 {
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE |
 		GLUT_RGBA);
@@ -34,7 +58,8 @@ int main(int argc, char **argv)
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("OpenGL Pierwsze Okno");
 	glewInit();
-	Init();
+	init2();
+
 	// rejestruj callbacks
 	glutDisplayFunc(renderScene);
 	glutMainLoop();
